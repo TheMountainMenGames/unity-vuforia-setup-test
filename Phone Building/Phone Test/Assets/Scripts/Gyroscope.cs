576e99a1c58b9d0317a28d088fdba255d8d8556f
2X2Q4657 <36763859+2X2Q4657@users.noreply.github.com>
2018-02-23 13:23:14 +0100

    Voor het eerste de project updated
    Jeeh als dit werkt

---
+++ Phone Building/Phone Test/Assets/Scripts/Gyroscope.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gyroscope : MonoBehaviour {
+
+    private bool gyroEnabled;
+    private Gyroscope gyro;
+
+    
+    // Use this for initialization
+	void Start () {
+        gyroEnabled = enableGyro();
+	}
+	
+    private bool enableGyro()
+    {
+        if (SystemInfo.supportsGyroscope)
+        {
+            gyro = Input.gyro;
+            gyro.enabled = true;
+            return true;
+        }
+
+        return false;
+
+    }
+	// Update is called once per frame
+	 Update () {
+
+	}
+}
